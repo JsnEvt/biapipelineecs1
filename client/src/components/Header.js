b478236daea09b50da5e5137c9ae0fc7c38ae993
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-05-14 14:38:45 -0300

    alteracao header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "green"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
       )}
